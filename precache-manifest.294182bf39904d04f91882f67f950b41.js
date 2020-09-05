819d665913a2c21f6ba4a96c0513789dc31b2fdb
Yuvrajhere <yuvrajisbest13@gmail.com>
2020-09-05 17:25:43 +0530

    Updates

---
+++ precache-manifest.294182bf39904d04f91882f67f950b41.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3726903fa73788e13b93cfa6eb7df4c",
+    "url": "/which-lang/index.html"
+  },
+  {
+    "revision": "e6cfc7cd94ef8142ac46",
+    "url": "/which-lang/static/css/main.fd96a3b1.chunk.css"
+  },
+  {
+    "revision": "f465ef0e9db896e400dd",
+    "url": "/which-lang/static/js/2.7bffaa5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/which-lang/static/js/2.7bffaa5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6cfc7cd94ef8142ac46",
+    "url": "/which-lang/static/js/main.8eb7d713.chunk.js"
+  },
+  {
+    "revision": "a726d70767eeffef4896",
+    "url": "/which-lang/static/js/runtime-main.a510028b.js"
+  },
+  {
+    "revision": "5ec7905887712a7249bcd3de301818ff",
+    "url": "/which-lang/static/media/about-svg.5ec79058.svg"
+  },
+  {
+    "revision": "9af2c8229ee8c5a957977faaf8d5a608",
+    "url": "/which-lang/static/media/error-svg.9af2c822.svg"
+  },
+  {
+    "revision": "344c44261d46d05e70ef0f842d71430b",
+    "url": "/which-lang/static/media/github-png.344c4426.png"
+  },
+  {
+    "revision": "dda47b59de4750407c68834edfee2516",
+    "url": "/which-lang/static/media/home-svg.dda47b59.svg"
+  },
+  {
+    "revision": "3827520cc2484eb161f0266d82313c0a",
+    "url": "/which-lang/static/media/instagram-png.3827520c.png"
+  },
+  {
+    "revision": "f989a4a9b0e24d257225d2b8d2cf02a2",
+    "url": "/which-lang/static/media/linkedin-png.f989a4a9.png"
+  },
+  {
+    "revision": "6404ee17050771004178df9d09d07484",
+    "url": "/which-lang/static/media/loading-gif.6404ee17.gif"
+  },
+  {
+    "revision": "1fe9c21dd28c2ce938610589e1570528",
+    "url": "/which-lang/static/media/start-svg.1fe9c21d.svg"
+  },
+  {
+    "revision": "e35b3314649437d84a0626025eff0054",
+    "url": "/which-lang/static/media/twitter-png.e35b3314.png"
+  }
+]);
